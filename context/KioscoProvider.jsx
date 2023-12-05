b3b25d5eeaecad206b0775e6536e489459e3f7f2
CescDevGit07 <cesar04071997@gmail.com>
2023-12-04 20:16:44 -0600

    Guardando cambios

--- context/KioscoProvider.jsx
+++ context/KioscoProvider.jsx
@@ -23,7 +23,7 @@ const pedidoLocalStorage = typeof window !== 'undefined' ? JSON.parse(localStora
     const router = useRouter()
 
     const obtenerCategorias = async () => {
-        const {data} = await axios('/api/categorias')
+        const {data} = await axios('/api/Categorias')
 
         setCategorias(data)
     }
